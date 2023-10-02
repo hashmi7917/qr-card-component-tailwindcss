7d7124e339f17b59dc89aeabb13166e2558ed791
Hashmi <imran@Hashmis-MacBook-Air.local>
2023-10-03 01:04:24 +0530

    prduct-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
 function App() {
-  return <Card />;
+  return (
+    <div className="w-80 max-w-screen-sm mx-auto shadow-2xl rounded-2xl bg-white py-6 h-full">
+      <img
+        className="w-auto rounded-t-2xl"
+        src="https://github.com/hashmi7917/product-preview-card-component-main/blob/main/images/image-product-mobile.jpg?raw=true"
+        alt="product-image"
+      />
+      <div className="flex flex-col justify-evenly py-2 px-6 h-96 min-h-min">
+        <h6 className="uppercase text-slate-500 tracking-widest font-light">
+          Perfume
+        </h6>
+        <h1 className="text-3xl font-fraunces">
+          Gabrielle Essence Eau De Parfum
+        </h1>
+        <p className="font-light">
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL.
+        </p>
+        <div>
+          <h2>
+            $149.99 <span>$169.99</span>
+          </h2>
+        </div>
+        <button className="bg-green-700 text-white font-medium">
+          <i></i> Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 }
 
 function Card() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          darkcyan: 'hsl(158, 36%, 37%)',
+          cream: 'hsl(30, 38%, 92%)',
+          verydarkblue: 'hsl(212, 21%, 14%)',
+          darkgreyishblue: 'hsl(228, 12%, 48%)',
+        },
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        fraunces: ['Fraunces', 'serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
