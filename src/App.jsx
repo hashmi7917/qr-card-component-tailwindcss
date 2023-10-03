fd1d448e5eb118dad8793c3e72e31d603dbb96a4
hashmi7917 <hmuqtadeer@gmail.com>
2023-10-03 19:36:36 +0530

    styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
 function Card() {
   return (
     <div className="max-w-sm bg-slate-50 rounded-2xl hover:shadow-xl">
-      <div className="max-w-sm p-4 shadow-2xl rounded-2xl">
+      <div className="max-w-sm p-4 shadow-2xl rounded-2xl h-full">
         <img
           className="w-6/24 rounded-2xl"
           src="https://hashmi7917.github.io/Frontend-Menoter-QR-Challenge-Completed/images/image-qr-code.png"
           alt="qr-image"
         />
-        <div className="h-52 flex flex-col justify-between text-center p-6">
-          <h1 className="text-2xl font-bold text-slate-950">
+        <div className="h-60 flex flex-col justify-between text-center px-6 py-4">
+          <h1 className="text-2xl font-bold text-slate-950 py-2">
             Improve your front-end skills by building projects
           </h1>
           <p className="text-slate-500 text-lg">
